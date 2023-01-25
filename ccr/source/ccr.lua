871f37bbb7798728614752735fbf9aa768b6ea69
TheJuiceFR <thejuiceirl@gmail.com>
2023-01-25 13:45:36 -0600

    9

--- ccr/source/ccr.lua
+++ ccr/source/ccr.lua
@@ -3,24 +3,35 @@ local tArgs={...}
 local option=tArgs[1]
 
 local function usageText()
-print([[Usage:
-
-
-
+print([[Usage: ccr <option> [arguments]
+	ccr install <package1> [package2]...
+		installs listed package(s)
+	ccr remove <package1> [package2]...
+		removes listed package(s)
+	ccr update
+		updates all packages
+	ccr info <package>
+		gives info about package
+	ccr list
+		lists installed packages
+	ccr listall
+		lists all available packages
 ]])
 end
 
 
 if option=="install" then
-
-elseif option=="update" then
-
+	
 elseif option=="remove" then
-
+	
+elseif option=="update" then
+	
 elseif option=="info" then
-
+	
 elseif option=="list" then
-
+	
+elseif option=="listall" then
+	
 else
 	usageText()
 end
